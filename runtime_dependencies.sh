d96175bfea54aa34b9df2a05451ba87cd22e4280
DoraemonX <75873265+doraemon99-x@users.noreply.github.com>
2024-07-22 02:23:48 +0700

    Update runtime_dependencies.sh

--- runtime_dependencies.sh
+++ runtime_dependencies.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-# Skrip untuk menginstall dependencies saat runtime
-pip install --no-cache-dir -r requirements.txt
+#!/bin/sh
+
+# Install python3-pip jika belum terinstall
+apt-get update && apt-get install -y python3-pip
+
+# Install dependencies dari requirements.txt
+pip3 install -r requirements.txt
+
+# Jalankan kode Python
+python3 -m main

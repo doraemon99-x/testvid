0e48da1fb391e8e9912a181870a3d45a91717eff
DoraemonX <75873265+doraemon99-x@users.noreply.github.com>
2024-07-22 00:45:08 +0700

    Update vid.py

--- vid.py
+++ vid.py
@@ -24,7 +24,7 @@ def job(url):
 tasks = [
     {
         "url": "https://tipiku.my.id/vidmpd/tv.php?id=6685",
-        "time": "00:38",
+        "time": "00:58",
         "date": "2024-07-22"
     },
     {

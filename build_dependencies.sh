6c50e76bff2e351a58d36f9fd44aa4b9455ecaf1
DoraemonX <75873265+doraemon99-x@users.noreply.github.com>
2024-07-22 02:17:51 +0700

    Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-
-# Memperbarui daftar package
-sudo apt-get update
-
-# Menginstall pip jika belum terinstall
-sudo apt-get install -y python3-pip
-
-# Menginstall virtualenv jika belum terinstall
-pip3 install virtualenv
-
-# Membuat virtual environment baru
-python3 -m venv venv
-
-# Mengaktifkan virtual environment
-source venv/bin/activate
-
-# Menginstall dependencies dari requirements.txt
-pip install -r requirements.txt
-
-echo "Setup selesai. Virtual environment telah diaktifkan."
-echo "Untuk mengaktifkan kembali virtual environment, gunakan: source venv/bin/activate"
+# Skrip untuk menginstall dependencies saat build
+pip install --no-cache-dir -r requirements.txt
